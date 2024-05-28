687efe40dd07997ffae2abd347f15cad89babbbc
George T Chakama <37905683+gtchakama@users.noreply.github.com>
2024-05-29 00:25:40 +0200

    chore: Update getUserDetails function to include first and last name

--- util/userDetails.js
+++ util/userDetails.js
@@ -1,11 +1,13 @@
 // Get all user details from session storage and return them as an object
 export const getUserDetails = () => {
   const userDetails = {
-    username: sessionStorage.getItem('username') || "User",
+    username: sessionStorage.getItem('username') || "",
     email: sessionStorage.getItem('email') || "",
-    role: sessionStorage.getItem('user_role') || "guest",
+    role: sessionStorage.getItem('user_role') || "",
     id: sessionStorage.getItem('user_id') || "",
     isAuthenticated: sessionStorage.getItem('isAuthenticated') === 'true', // Convert to boolean
+    firstName: sessionStorage.getItem('first_name') || "Guest User",
+    lastName: sessionStorage.getItem('last_name') || "",
   };
 
   return userDetails;
