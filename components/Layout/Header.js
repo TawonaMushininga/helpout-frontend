2a5d47be1ff07fc8c62e8da737af74df005478e7
George T Chakama <37905683+gtchakama@users.noreply.github.com>
2024-05-29 00:25:07 +0200

    chore: Update create job form submission to include session headers

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -7,10 +7,37 @@ import Link from 'next/link';
 import React from 'react';
 import { getUserDetails } from "../../util/userDetails";
 
-const Header = ({handleOpen,handleRemove,openClass}) => {
+const Header = ({ handleOpen, handleRemove, openClass }) => {
     const [scroll, setScroll] = useState(false);
     const [userDetails, setUserDetails] = useState({});
-    // const { isAuthenticated } = typeof window !== "undefined" ? getUserDetails() : { isAuthenticated: false };
+    const [isAuth, setIsAuth] = useState(false);
+
+    const isAuthenticated = async () => {
+        const accessToken = await sessionStorage.getItem('access-token');
+        if (accessToken !== null) {
+            console.log('authenticated');
+            return true;
+        }
+        console.log('not authenticated');
+        return false;
+    }
+
+    const logout = async () => {
+        await sessionStorage.removeItem('access-token');
+        await sessionStorage.removeItem('refresh-token');
+        await sessionStorage.removeItem('user_id');
+        await sessionStorage.removeItem('username');
+        await sessionStorage.removeItem('email');
+        await sessionStorage.removeItem('user_role');
+        await sessionStorage.removeItem('isAuthenticated');
+        await sessionStorage.removeItem('first_name');
+        await sessionStorage.removeItem('last_name');
+        await sessionStorage.removeItem('user_role');
+
+        window.location.href = "/page-signin";
+    }
+
+    // isAuthenticated().then(result => console.log(result));
 
     useEffect(() => {
         const handleScroll = () => {
@@ -22,11 +49,21 @@ const Header = ({handleOpen,handleRemove,openClass}) => {
         };
     }, []);
 
-    // useEffect(() => {
-    //     if (isAuthenticated) {
-    //         setUserDetails(getUserDetails());
-    //     }
-    // }, [isAuthenticated]);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const auth = await isAuthenticated();
+            setIsAuth(auth);
+        };
+
+        checkAuth();
+    }, []);
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            setUserDetails(getUserDetails());
+        }
+    }, [isAuth]);
 
     return (
         <>
@@ -35,17 +72,18 @@ const Header = ({handleOpen,handleRemove,openClass}) => {
                     <div className="main-header">
                         <div className="header-left">
                             <div className="header-logo">
-                            <Link legacyBehavior href="/"><a className="d-flex"><img alt="jobBox" src="assets/imgs/template/jobhub-logo.svg" /></a></Link>
+                                <Link legacyBehavior href="/"><a className="d-flex"><img alt="jobBox" src="assets/imgs/template/jobhub-logo.svg" /></a></Link>
                             </div>
                         </div>
                         <div className="header-nav">
                             <nav className="nav-main-menu">
                                 <ul className="main-menu">
                                     <li className="">
-                                    <Link legacyBehavior href="/"><a className="active">Home</a></Link>
+                                        <Link legacyBehavior href="/"><a className="active">Home </a></Link>
                                     </li>
                                     <li className="">
-                                        <Link legacyBehavior href="/jobs-grid"><a>Find a Job</a></Link>
+
+                                        <Link legacyBehavior href={isAuth == true ? "/jobs-grid" : "/page-signin"}><a>GiGs</a></Link>
 
                                     </li>
                                     <li className="">
@@ -54,7 +92,7 @@ const Header = ({handleOpen,handleRemove,openClass}) => {
                                     </li>
 
                                     <li className="">
-                                        <Link legacyBehavior href="/candidate-details"><a>Profile</a></Link>
+                                        <Link legacyBehavior href={isAuth == true ? "/candidate-details" : "/page-signin"}><a>Profile</a></Link>
 
                                         {/* <ul className="sub-menu">
                                             <li>
@@ -104,27 +142,38 @@ const Header = ({handleOpen,handleRemove,openClass}) => {
                                 </ul>
                             </nav>
                             <div className={`burger-icon burger-icon-white ${openClass && "burger-close"}`}
-                            onClick={()=>{handleOpen(); handleRemove()}}>
+                                onClick={() => { handleOpen(); handleRemove() }}>
                                 <span className="burger-icon-top" /><span className="burger-icon-mid" /><span className="burger-icon-bottom" /></div>
                         </div>
                         <div className="header-right">
                             <div className="block-signin">
-                                {/* {
-                                    isAuthenticated ? (
-                                        <Link legacyBehavior href="/page-profile"><a className="text-link-bd-btom hover-up">Welcome, {userDetails.username}</a></Link>
+                                {
+                                    isAuth == true ? (
+                                        <div style={{
+                                            display: "flex",
+                                            alignItems: "center"
+                                        }}>
+                                            <Link legacyBehavior href="/page-profile"><a className="text-link-bd-btom hover-up">Welcome, {userDetails.firstName}</a></Link>
+                                            <button onClick={() => logout()}
+                                                style={{
+                                                    cursor: "pointer",
+                                                    color: "#fff",
+                                                    backgroundColor: "#f00",
+                                                    padding: "4px 10px",
+                                                    borderRadius: "5px",
+                                                    border: "none",
+                                                    marginLeft: "10px"
+
+                                                }}
+                                            >Logout</button>
+                                        </div>
                                     ) : (
                                         <>
                                             <Link legacyBehavior href="/page-signin"><a className="text-link-bd-btom hover-up bg-blue-500 p-4">Sign In</a></Link>
                                             <Link legacyBehavior href="/page-register"><a className="text-link-bd-btom hover-up p-4">Register</a></Link>
                                         </>
                                     )
-                                } */}
-
-
-<>
-                                            <Link legacyBehavior href="/page-signin"><a className="text-link-bd-btom hover-up bg-blue-500 p-4">Sign In</a></Link>
-                                            <Link legacyBehavior href="/page-register"><a className="text-link-bd-btom hover-up p-4">Register</a></Link>
-                                        </>
+                                }
 
                             </div>
                         </div>
